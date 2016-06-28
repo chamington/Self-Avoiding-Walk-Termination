2a5ef1c531e2bdc7ef4500632f372a04bb85baa0
chamington <cosmictartar@gmail.com>
2016-06-28 16:00:07 -0400

    idk lol

--- firstproject.py
+++ firstproject.py
@@ -34,34 +34,86 @@ for i in range(0, 10):
 	x=250
 	y=250
 	i=0
-	bordercreation=1
-	turtle.setx((0))
+	trapped = False
+	turtle.setx(0)
 	turtle.sety(0)
 	turtle.pendown()
 	trapped=False
 	Matrix = [[0 for ax in range(500)] for ay in range(500)]
-	for border in range (0,500):
-		Matrix[bordercreation,1]=3
-		Matrix[bordercreation,500]=3
-		Matrix[0,bordercreation]=3
-		Matrix[500,bordercreation]=3
-		bordercreation=bordercreation+1
+	for border in range (2,498):
+		Matrix[border][498]=3
+		Matrix[border][2]=3
+		Matrix[2][border]=3
+		Matrix[498][border]=3
 	while nomove==False:
 		Matrix[250][250]=True
 		xory = random.choice([True,False])
 		porm = random.choice([-1,1])
-		Matrix2=Matrix
 		if xory==True:
 			if (Matrix[x+porm][y]==0):
-				x =x+ porm
-				Matrix[x][y]=1
+			#trapped = True
+				Matrix2=Matrix
+				if xory == True:
+					Matrix2[x+porm][y]=2
+				else:
+					Matrix2[x][y+porm]=2
+				for amount in range (0,50):
+					for xfill in range (1,499):
+						for yfill in range (1,499):
+							if(Matrix2[xfill][yfill]==2):
+								if Matrix2[xfill+1][yfill]==3:
+									trapped = False
+								if Matrix2[xfill-1][yfill]==3:
+									trapped = False
+								if Matrix2[xfill][yfill+1]==3:
+									trapped = False
+								if Matrix2[xfill][yfill-1]==3:
+									trapped = False
+								if Matrix2[xfill+1][yfill]==0:
+									Matrix2[xfill+1][yfill]=2
+								if Matrix2[xfill-1][yfill]==0:
+									Matrix2[xfill-1][yfill]=2
+								if Matrix2[xfill][yfill+1]==0:
+									Matrix2[xfill][yfill+1]=2
+								if Matrix2[xfill][yfill-1]==0:
+									Matrix2[xfill][yfill-1]=2
+				if (trapped==False):
+					x =x+ porm
+					Matrix[x][y]=1
+					i+=1
 		if xory==False:
 			if (Matrix[x][y+porm]==0):
-				y =y+ porm
-				Matrix[x][y]=1
-				i += 1
-				turtle.setx((x-250)*5)
-				turtle.sety((y-250)*5)
+				#trapped = True
+				Matrix2=Matrix
+				if xory == True:
+					Matrix2[x+porm][y]=2
+				else:
+					Matrix2[x][y+porm]=2
+				for amount in range (0,50):
+					for xfill in range (1,499):
+						for yfill in range (1,499):
+							if(Matrix2[xfill][yfill]==2):
+								if Matrix2[xfill+1][yfill]==3:
+									trapped = False
+								if Matrix2[xfill-1][yfill]==3:
+									trapped = False
+								if Matrix2[xfill][yfill+1]==3:
+									trapped = False
+								if Matrix2[xfill][yfill-1]==3:
+									trapped = False
+								if Matrix2[xfill+1][yfill]==0:
+									Matrix2[xfill+1][yfill]=2
+								if Matrix2[xfill-1][yfill]==0:
+									Matrix2[xfill-1][yfill]=2
+								if Matrix2[xfill][yfill+1]==0:
+									Matrix2[xfill][yfill+1]=2
+								if Matrix2[xfill][yfill-1]==0:
+									Matrix2[xfill][yfill-1]=2
+
+				if (trapped==False):
+					y =y+ porm
+					Matrix[x][y]=1
+					i += 1
 		turtle.setx((x-250)*5)
 		turtle.sety((y-250)*5)
 		if (Matrix[x][y+1]==1 and Matrix[x][y-1]==1 and Matrix[x+1][y]==1 and Matrix[x-1][y]==1):
